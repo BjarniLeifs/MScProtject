b1394262a5f9a762b8a28fc63baccf168fb82db4
Bjarni Leifsson <bjarnil10@ru.is>
2017-08-22 15:37:18 +0000

    update

--- apidoc/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "title": "Documentation",  "url": "http://localhost:3001",  "name": "Uxinn",  "version": "1.0.0",  "description": "API",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-07-17T21:18:02.499Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
